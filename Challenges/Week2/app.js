221af0d81949ee82a9e7a77661c741831b93e3fe
paullouisville <pdcolon@gmail.com>
2016-09-07 20:03:03 -0400

    completed challenge

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -6,3 +6,18 @@
 //Call function
 
 //Alert user results
+
+function ProcessMessage(msg)
+{
+if(msg === "Hello")
+{
+  alert("Hello World")
+}
+console.log (msg);
+  }
+
+var message ;
+
+message = prompt();
+
+ProcessMessage (message);
